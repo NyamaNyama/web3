c0c2b31db5f38aa811852540c4cea45fa4ef0ab8
Nyama N <>
2024-01-11 13:56:12 +1000

    create models

--- project/photo_app/models.py
+++ project/photo_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    title = models.CharField(max_length=255)
+    tags = models.ManyToManyField(Tag, blank=True)
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
