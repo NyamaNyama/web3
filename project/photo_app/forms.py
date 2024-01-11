dfeae208df7acb29075497f1a202bbb8b4abc1b3
Nyama N <>
2024-01-11 15:56:28 +1000

    add image form

---
+++ project/photo_app/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from .models import Image, Tag
+
+class ImageForm(forms.ModelForm):
+    tags = forms.CharField(max_length=255, required=False,label="Теги", help_text='Введите теги через запятую')
+    title = forms.CharField(label="Заголовок", max_length=255)
+    image = forms.ImageField(label="Изображение")
+    class Meta:
+        model = Image
+        fields = ['title', 'image', 'tags']
+
+    def save(self, commit=True):
+        image = super(ImageForm, self).save(commit=False)
+
+        if commit:
+            image.save()
+
+        tag_names = [tag.strip() for tag in self.cleaned_data['tags'].split(',') if tag.strip()]
+
+        image.tags.clear()
+        for tag_name in tag_names:
+            tag, created = Tag.objects.get_or_create(name=tag_name)
+            image.tags.add(tag)
+
+        if commit:
+            image.save()
+        return image
+
+
+
+    class Meta:
+        model = Tag
+        fields = ['name']
+
+class PasswordForm(forms.Form):
+    password = forms.CharField(widget=forms.PasswordInput)
